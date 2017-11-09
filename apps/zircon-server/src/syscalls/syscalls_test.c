1ff9746ab770ba8080736bbd81a71418057a8984
Jack Suann <Jack.Suann@data61.csiro.au>
2017-11-09 17:38:54 +1100

    Syscall table now stored individually in sys_table.c

--- apps/zircon-server/src/syscalls/syscalls_test.c
+++ apps/zircon-server/src/syscalls/syscalls_test.c
@@ -7,6 +7,14 @@
 
 #include "syscalls.h"
 
+void sys_null(seL4_MessageInfo_t tag, uint32_t handle)
+{
+    printf("Got null syscall! %u\n", handle);
+    printf("Num args: %lu\n", seL4_MessageInfo_get_length(tag));
+    tag = seL4_MessageInfo_new(0, 0, 0, 0);
+    seL4_Reply(tag);
+}
+
 #define DO_TEST_SYSCALL(n) \
     printf("sys test %d\n", n); \
     if (seL4_MessageInfo_get_length(tag) != n) { \
